10365fde75b31b697fbdb899467d40d55b026571
Ustad Nordin <bouchtaoui.dev@gmail.com>
2022-03-21 10:39:32 +0100

    variabelen behandeld

---
+++ variable.js
@@ -0,0 +1,27 @@
+// Vroeger werd een variabele gedeclareerd met 'var'
+var naam = "Johan"; // <-- var niet meer gebruiken!
+
+// Correcte manier
+let naam = "Johan"; // <-- dit is de juiste manier!
+
+const versie = "1.0.0"; // <-- dit is een constante
+const PI = 3.14159; // PI verandert niet, dus een constante!
+
+
+// type variabelen:
+-80, -3, 0, 1, 13, 167 // Deze zijn integers (oftewel hele getallen)
+// Voorbeeld
+let aantal_items = 75;
+print("Aantal items is: ", aantal_items);
+
+-67.54, 0.012, 2.30, 4.563278 // Deze zijn floats (getallen met drijvende komma's)
+let gewicht = 73.50;
+print("Mijn gewicht is: ", gewicht);
+
+"g", "Voer uw naam in: ", "21/03/2022" // Deze zijn strings
+let organisatie = "ROC Mondriaan";
+print("Aantal items is: ", organisatie);
+
+true, false // booleans, dus waar of niet waar
+let is_geregistreerd = false;
+print("Bent u geregistreerd?: ", is_geregistreerd);
